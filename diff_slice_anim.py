eb6e978b3417170ff822b6b7c5d3172241818c26
jpolton <jelt@noc.ac.uk>
2018-06-07 15:18:12 +0100

    generalise surface variable

--- diff_slice_anim.py
+++ diff_slice_anim.py
@@ -26,7 +26,6 @@ Aspirational usage (maybe) diff_slice_anim.py ifile1 ifile2 var ofile
 from netCDF4 import Dataset
 import numpy as np
 import matplotlib.pyplot as plt  # plotting
-import matplotlib.cm as cm  # colormaps
 import sys
 #sys.path.append('../jcomp_tools_dev/') # Add the directory with the amm60_data_tools.py file to path. Not used on the NOCL SAN
 from AMM60_tools import NEMO_fancy_datestr # NEMO date strings
@@ -37,53 +36,23 @@ import datetime
 
 ## Read in the input parameters
 
-#vtype = 'ssh'
-#vtype = 'sst'
-#vtype = 'ssu'
-vtype = 'ssv'
-
 #ifile1 = sys.argv[1]
 #ifile2 = sys.argv[2]
 #rootdir = "/Users/jeff/Desktop/OneWeekExpiry/SEAsia/"
-#rootdir = "/work/n01/n01/jdha/2018/SEAsia/EXP_openbcs/"
-rootdir = "/scratch/jdha/SEAsia/"
+rootdir = "/work/n01/n01/jdha/2018/SEAsia/EXP_openbcs/"
 #ifile1 = "OUTPUT_SPLIT_NONE/SEAsia_1d_19791101_19791130_grid_T.nc"
 #ifile2 = "OUTPUT_FULL2_NONE/SEAsia_1d_19791101_19791130_grid_T.nc"
 
 EXPa = "EXP4"
 EXPb = "EXP5"
 
-freq = '5d'
-if vtype == 'ssh':
-        grid = 'grid_T'
-        freq = '1d'
-	variable = 'zos'
-	levs = np.arange(-2,2+0.2,0.2)
-	dlevs = np.arange(-0.1,0.1+0.01,0.01)
-elif vtype == 'sst':
-        grid = 'grid_T'
-        variable = 'toce'
-	levs = np.arange(20,30+1,1)
-	dlevs = np.arange(-1,1+0.1,0.1)
-elif vtype == 'ssu':
-	grid = 'grid_U'
-        variable = 'uoce'
-	levs = np.arange(-2,2+0.2,0.2)
-	dlevs = np.arange(-0.1,0.1+0.01,0.01)
-elif vtype == 'ssv':
-	grid = 'grid_V'
-        variable = 'voce'
-	levs = np.arange(-2,2+0.2,0.2)
-	dlevs = np.arange(-0.1,0.1+0.01,0.01)
+ifile1 = "OUTPUT_" + EXPa + "/SEAsia_1d_19791101_19791130_grid_T.nc"
+ifile2 = "OUTPUT_" + EXPb + "/SEAsia_1d_19791101_19791130_grid_T.nc"
 
+variable = 'sea_surface_temperature'
 
- 
-mfile = "OUTPUT_EXP4/SEAsia_5d_19791101_19791130_grid_T.nc"
-ifile1 = "OUTPUT_" + EXPa + "/SEAsia_" + freq + "_19791101_19791130_" + grid + ".nc"
-ifile2 = "OUTPUT_" + EXPb + "/SEAsia_" + freq + "_19791101_19791130_" + grid + ".nc"
-
-ofile = 'FIGURES/SEAsia_' + EXPa + '_' + EXPb + '_' + vtype + '_slice.gif'
-
+ofile = 'FIGURES/SEAsia_' + EXPa + '_' + EXPb + '_' + variable + '_slice.gif'
+levs = np.arange(20,30+1,1)
 #levs = [-1.5,-1,-0.5,0,0.5,1,1.5]
     
 #variable = 'zos'
@@ -91,7 +60,6 @@ ofile = 'FIGURES/SEAsia_' + EXPa + '_' + EXPb + '_' + vtype + '_slice.gif'
 #levs = np.arange(-2,2+0.1,0.1)
 
 
-f0 = Dataset(rootdir + mfile)
 f1 = Dataset(rootdir + ifile1)
 f2 = Dataset(rootdir + ifile2)
 
@@ -99,19 +67,8 @@ xlim = [75,135]
 ylim = [-20.,20.]
 
 ## Load in data
-mvar = f0.variables['toce'][0,0,:,:] 
 var1 = f1.variables[variable][:] # Size? e.g. (t, y, x)  
 var2 = f2.variables[variable][:] # Size? e.g. (t, y, x)  
-
-## Deduce the number of dimensions in var and take surface if spatially 3D
-if len(np.shape(var1)) == 4:
-	var1 = np.squeeze(var1[:,0,:,:])
-	var2 = np.squeeze(var2[:,0,:,:])
-
-[nt,ny,nx] = np.shape(var1)
-
-# mask land
-# to do
 
 print np.min(var1)
 print np.max(var1)
@@ -145,17 +102,15 @@ def make_gif(files,output,delay=100, repeat=True,**kwargs):
     
 
 def sliceplotshade(X,Y,var,count,xlim,ylim,levs):
-    cmap = cm.Spectral_r
-    cmap.set_bad('white',1.)
-    cmap.set_over('red',1.)
-    cset = ax.pcolormesh(X, Y, var[count,:,:], cmap=cmap)
+
+    cset = ax.pcolormesh(X, Y, var[count,:,:], cmap='Spectral')
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     #ax.set_xlabel('\nlongitude (deg)')
     #ax.set_ylabel('\nlatitude (deg)')
     cset.set_clim([levs[0],levs[-1]])                    
     ax.hold(True)
-    #cs = ax.contour(X, Y, var[count,:,:], levels=levs, colors='k')
+    cs = ax.contour(X, Y, var[count,:,:], levels=levs, colors='k')
     
     fig.colorbar(cset) # Add colorbar
     adjustFigAspect(fig,aspect=(xlim[1]-xlim[0])/(ylim[1]-ylim[0]))
@@ -185,15 +140,17 @@ if __name__ == '__main__':
     
     count = 1
 
-
+    [nt,ny,nx] = np.shape(var1)
 
     X_arr = nav_lon
     Y_arr = nav_lat
+    var_arr = var1[:,:,:] # Note this also has time dimension
 
+    var_arr[var1==0] = np.nan 
     files = []
 
     ## Step through frames
-    for count in range(nt-1,nt):
+    for count in range(1,5):
         print count
 
         plt.close('all')
@@ -202,7 +159,7 @@ if __name__ == '__main__':
         # Plot var1 
         plt.subplot(311)
         ax = fig.gca()
-        sliceplotshade(X_arr,Y_arr,var1,count,xlim,ylim,levs)   
+        sliceplotshade(X_arr,Y_arr,var1[:,:,:],count,xlim,ylim,levs)   
         dat = datetime.datetime.strftime(time_datetime[count], '%d %b %Y: %H:%M')
         plt.title('a) '+ifile1)
         ax.text(80, 18, str(dat), fontsize=10)
@@ -213,7 +170,7 @@ if __name__ == '__main__':
         # Plot var2 
         plt.subplot(312)
         ax = fig.gca()
-        sliceplotshade(X_arr,Y_arr,var2,count,xlim,ylim,levs)   
+        sliceplotshade(X_arr,Y_arr,var2[:,:,:],count,xlim,ylim,levs)   
         dat = datetime.datetime.strftime(time_datetime[count], '%d %b %Y: %H:%M')
         plt.title('b) '+ifile2)
         ax.text(80, 18, str(dat), fontsize=10)
@@ -224,7 +181,7 @@ if __name__ == '__main__':
         # Plot var1 - var2 
         plt.subplot(313)
         ax = fig.gca()
-        sliceplotshade(X_arr,Y_arr,var1-var2,count,xlim,ylim,dlevs)   
+        sliceplotshade(X_arr,Y_arr,var1-var2,count,xlim,ylim,np.arange(-1,1+0.1,0.1))   
         dat = datetime.datetime.strftime(time_datetime[count], '%d %b %Y: %H:%M')
         plt.title('(a)-(b)')
         ax.text(80, 18, str(dat), fontsize=10)
