8456babebfd0da9a051196a90279f4d6a7c85db4
jpolton <jelt@noc.ac.uk>
2018-06-14 21:49:23 +0100

    tidy up

--- Solent_SSH_anim_zoom.py
+++ Solent_SSH_anim_zoom.py
@@ -6,7 +6,7 @@
 """
 ** Summary: **
 Build 2D animation of Solent SSH with velocity
-Load in some existing output Solent_surge_5mi_20130101_20130101_5min.nc. Plot it. Animate. 
+Load in some existing output Solent_surge_5mi_20130101_20130101_5min.nc. Plot it. Animate.
 ** origin: ** cotidalchart_NEMO.ipynb
 ** Author: ** JP 31 Jan 2017
 ** Changelog: **
@@ -17,7 +17,7 @@ Load in some existing output Solent_surge_5mi_20130101_20130101_5min.nc. Plot it
 * 14 Jun 2019: NB - extend with zoom and time series - things about zoom box are actually hard coded; no basmap projection used for it
 
 ** Issues: **
-* The longitude label is missing from the final plots
+* None. It is amazing.
 """
 
 ##################### LOAD MODULES ###########################
@@ -42,27 +42,27 @@ font0  = FontProperties(); font = font0.copy(); font.set_weight('bold'); font.se
 
 ## Check host name and locate file path
 import socket
-if str('livmaf') in socket.gethostname():
-    dirname = '/Volumes/archer/jelt/NEMO/NEMOGCM_jdha/dev_r4621_NOC4_BDY_VERT_INTERP/NEMOGCM/CONFIG/XIOS_AMM60_nemo_harmIT2/EXP_harmIT2/OUTPUT/'
+if str('livmap') in socket.gethostname().lower():
+    dirname = '/Users/jeff/Desktop/'
 #    dirname = '/Users/jeff/Desktop/OneWeekExpiry/tmp/'
-elif str('livljobs') in socket.gethostname():
+elif str('livljobs') in socket.gethostname().lower():
     dirname = '/scratch/jelt/tmp/'
-elif str('eslogin') in socket.gethostname():
-    dirname = '/Volumes/archer/jelt/NEMO/NEMOGCM_jdha/dev_r4621_NOC4_BDY_VERT_INTERP/NEMOGCM/CONFIG/XIOS_AMM60_nemo_harmIT2/EXP_harmIT2/OUTPUT/'
+elif str('eslogin') in socket.gethostname().lower():
+    dirname = '/work/n01/n01/jelt/Solent_surge/dev_r8814_surge_modelling_Nemo4/CONFIG/Solent_surge/EXP_TPXO/'
 else:
     print 'There is no working ELSE option'
 
 #################### USR PARAMETERS ##########################
 
-dirname  = '/work/n01/n01/jelt/Solent_surge/dev_r8814_surge_modelling_Nemo4/CONFIG/Solent_surge/EXP_TPXO/' 
-filename = 'Solent_surge_TPXO_5mi_20130101_20130101_5min.nc' 
+#dirname  = '/work/n01/n01/jelt/Solent_surge/dev_r8814_surge_modelling_Nemo4/CONFIG/Solent_surge/EXP_TPXO/'
+filename = 'Solent_surge_TPXO_5mi_20130101_20130101_5min.nc'
 levs     = np.arange(-2.5,2.5+0.1,0.1)
 ofile    = 'FIGURES/Solent_SSH.gif'
 
 index_Buoys = [ [-200,700], [200,200], ]  ## Index iLon, iLat of each gauge point to extract time series
 out_every   = 1                          ## Generate graphic every X time step
 init_step   = 2                           ## which iteration to start outputting
-end_step    = 5                           ## stop output at nt-end_step    
+end_step    = 5                           ## stop output at nt-end_step
 color_Buoys = [ '#8d349b', 'b', 'g', 'r' ]
 
 #################### INTERNAL FCTNS #########################
@@ -82,10 +82,10 @@ def sliceplotshade( pj, X, Y, ssh, u, v, levs, tdate, \
 
     xx,yy = pj( X, Y )           ## Projecton Basemap
     speed = np.sqrt(u**2+v**2)   ## Compute   Currents
- 
+
     ## Create colorbar and nan color
     cmap0 = plt.cm.get_cmap('Spectral_r', 256)
-    cmap0.set_bad('#9b9b9b', 1.0) 
+    cmap0.set_bad('#9b9b9b', 1.0)
     cset  = pj.pcolormesh( xx, yy, ssh, cmap = cmap0 )
     cset.set_clim([levs[0],levs[-1]])
 
@@ -129,12 +129,12 @@ def sliceplotshade( pj, X, Y, ssh, u, v, levs, tdate, \
     zax.contourf( X, Y, ssh[:,:], levs, cmap=cmap1, zorder=100 )
     zax.quiver(  X[ ::vector_zoom_every, ::vector_zoom_every ][ind], Y[ ::vector_zoom_every, ::vector_zoom_every ][ind], \
                  u[ ::vector_zoom_every, ::vector_zoom_every ][ind], v[ ::vector_zoom_every, ::vector_zoom_every ][ind], \
-                 color='k' , scale=30, alpha=0.9, zorder=101 ) 
+                 color='k' , scale=30, alpha=0.9, zorder=101 )
     zax.set_xlim([boxlonmin,boxlonmax]); zax.set_ylim([boxlatmin,boxlatmax])
     zax.tick_params(axis='x',labelbottom='off', bottom=False, top  =False )
     zax.tick_params(axis='y',labelleft  ='off', left  =False, right=False )
 
-    ## Colorbar 
+    ## Colorbar
     cax  = plt.axes([0.2, 0.08, 0.6, 0.02])
     cbar = plt.colorbar( cset, cax=cax, orientation='horizontal', extend='both'  )
     cbar.ax.tick_params( labelsize=12 )
@@ -156,7 +156,7 @@ vbar = np.ma.array( vbar, mask = mask )
 ## Subsampled every X time step (faster)
 ssh  = ssh [ init_step::out_every ]
 mask = mask[ init_step::out_every ]
-ubar = ubar[ init_step::out_every ] 
+ubar = ubar[ init_step::out_every ]
 vbar = vbar[ init_step::out_every ]
 spd  = np.sqrt(ubar**2+vbar**2) ## Compute norm currents
 print "MAX SPEED", np.nanmax( spd )
@@ -189,20 +189,18 @@ if __name__ == '__main__':
     [nt,ny,nx] = np.shape( ssh )
     X_arr   = nav_lon
     Y_arr   = nav_lat
-    var_arr = ssh[:,:,:] # Note this also has time dimension
     files   = []
 
-    print 'Hardwired timestep in variable dat'
     ## Time timeseries of frames
     #for count in range(nt-25,nt):
     print time_datetime[0], time_datetime[nt-end_step-1]
     print dats[0], dats[nt-end_step-1]
     for count in range(0,nt-end_step):  # By eye this looks like a closed cycle
         #dat = datetime.datetime.strftime(time_datetime[count], '%d %b %Y: %H:%M')
-        datsec = (time_datetime[count] - time_datetime[0]).total_seconds() 
+        datsec = (time_datetime[count] - time_datetime[0]).total_seconds()
         dath = int( datsec/3600 )
         datm = int( (datsec - dath * 3600) / 60 )
-        dat  = "{0:02d}:{1:02d}".format( dath, datm ) 
+        dat  = "{0:02d}:{1:02d}".format( dath, datm )
 
         # DEFINE FIGURES AND SUBGRID
         fig = plt.figure( figsize = ( 8.4, 2+10*(Latmax-Latmin)/(Lonmax-Lonmin) ) )
@@ -216,19 +214,19 @@ if __name__ == '__main__':
 
         sliceplotshade( pj, X_arr, Y_arr, ssh[count], ubar[count], vbar[count], levs, str(dat), \
                         streamline = False, maxcurrent = np.nanmax(spd), \
-                        vectorevery = 50, vector_zoom_every = 2 ) 
+                        vectorevery = 50, vector_zoom_every = 2 )
 
         plt.title('Sea Surface Height (m)', fontsize=12, fontproperties=font )
 
         ## PLOT GAUGE
-        for iB, Buoy in enumerate( index_Buoys ) :    
+        for iB, Buoy in enumerate( index_Buoys ) :
             ax.plot( X_arr[ Buoy[1], Buoy[0] ], Y_arr[ Buoy[1], Buoy[0] ], \
                      'bo', color=color_Buoys[iB], markersize=10, zorder=100, alpha=0.9 )
 
         ## DEFINE TIME SERIE FIGURES
         ax1 = plt.axes([0.12, 0.82, 0.87, 0.16])
         for iB, Buoy in enumerate( index_Buoys ) :
-            ax1.plot( dats, ssh[:, Buoy[1], Buoy[0] ], 'b-', color=color_Buoys[iB], linewidth = 2 )        
+            ax1.plot( dats, ssh[:, Buoy[1], Buoy[0] ], 'b-', color=color_Buoys[iB], linewidth = 2 )
         ax1.plot( [dats[count],dats[count]],[-3,3.45], 'k-', linewidth = 3 )
         ax1.set_ylabel( "SSH (m)", fontsize=12, fontproperties=font )
 #        ax1.set_xlim( [time_datetime[1],time_datetime[nt-end_step-1]] )
@@ -241,7 +239,7 @@ if __name__ == '__main__':
         print str(dat),fname
         plt.savefig(fname, dpi=100)
         plt.close()
-        
+
         files.append(fname)
 
     # Make the animated gif and clean up the files
